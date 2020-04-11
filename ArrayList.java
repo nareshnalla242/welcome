11b64a08ca8fe069d9c0e0e7bc8f42b630126108
naresh <nareshnalla456@gmail.com>
2020-04-11 15:25:36 +0530

    first commit

---
+++ ArrayList.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class ArrayList{
+public void main(String[]ar){
+ArrayList<String> al=new  ArrayList<String>();
+al.add("nokia");
+al.add("samsung");
+al.add("sony");
+al.add("htc");
+al.add("motorola");
+al.add("oppo");
+al.add("vivo");
+System.out.println("list:"+al);
+}
+}
